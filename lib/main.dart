31703fb4c4359edc7888015c9a63f79663794574
Sergei Tskhovrebov <sinxwal@gmail.com>
2022-09-22 21:39:29 +0300

    dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/app.dart';
 import 'src/bloc/harry_collections_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
   final harryCollectionsBloc = HarryCollectionsBloc();
 
   harryCollectionsBloc.add(const HarryCollectionsEvent.bootstrap());
--- lib/src/bloc/harry_collections_bloc.dart
+++ lib/src/bloc/harry_collections_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../models/elixir.dart';
 
@@ -10,6 +11,9 @@ part 'harry_collections_bloc.freezed.dart';
 
 class HarryCollectionsBloc extends Bloc<HarryCollectionsEvent, HarryCollectionsState> {
   HarryCollectionsBloc() : super(const _Initial()) {
+    // ignore: non_constant_identifier_names
+    final BASE_URL = dotenv.env['BASE_URL'];
+
     on<HarryCollectionsEvent>((event, emit) async {
       var dio = Dio();
 
@@ -17,15 +21,13 @@ class HarryCollectionsBloc extends Bloc<HarryCollectionsEvent, HarryCollectionsS
         emit(const HarryCollectionsState.loading());
 
         try {
-          final response = await dio.get<List<dynamic>>('https://wizard-world-api.herokuapp.com/Elixirs');
+          final response = await dio.get<List<dynamic>>('$BASE_URL/Elixirs');
           if (response.statusCode == 200) {
             var elixirs = <Elixir>[];
 
-            (response.data)?.forEach((v) {
-              try {
-                elixirs.add(Elixir.fromJson(v));
-              } catch (e) {}
-            });
+            for (final v in response.data as List<dynamic>) {
+              elixirs.add(Elixir.fromJson(v));
+            }
 
             emit(HarryCollectionsState.loaded(elixirs));
           }
