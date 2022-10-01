4a0b165f29f3e387027359f82e54737b4d20a70c
Sergei Tskhovrebov <sinxwal@gmail.com>
2022-10-02 02:08:20 +0300

    добавил вывод списка эликсиров

--- lib/ui/routes/Elixirs/elixirs_page.dart
+++ lib/ui/routes/Elixirs/elixirs_page.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../application/bloc/harry_collections_bloc.dart';
+import '../../../data/models/elixir.dart';
 
 class ElixirsPage extends StatelessWidget {
   const ElixirsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('Elixirs Page Route'),
+    return BlocBuilder<HarryCollectionsBloc, HarryCollectionsState>(
+      builder: (context, state) {
+        return state.maybeMap(
+          orElse: () {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
+              ),
+            );
+          },
+          loaded: ((data) {
+            return ListView.builder(
+              itemBuilder: ((BuildContext context, int index) {
+                final item = data.elixirs[index];
+                return ListTile(
+                  title: Text(item.name != null ? item.name as String : ''),
+                  trailing: _renderDifficultyLabel(item),
+                  subtitle: _renderIngredientsLabel(item),
+                  isThreeLine: true,
+                );
+              }),
+              itemCount: data.elixirs.length,
+            );
+          }),
+        );
+      },
     );
+  }
+
+  Widget _renderDifficultyLabel(Elixir elixir) {
+    String label = '?';
+    switch (elixir.difficulty) {
+      case 'Beginner':
+        label = '*';
+        break;
+      case 'Moderate':
+        label = '***';
+        break;
+      case 'Advanced':
+        label = '*****';
+        break;
+    }
+    return Text(
+      label,
+      style: const TextStyle(fontSize: 20.0),
+    );
+  }
+
+  Widget _renderIngredientsLabel(Elixir elixir) {
+    String label = elixir.ingredients!.map((e) => e.name).join(', ');
+    return Text(label);
   }
 }
